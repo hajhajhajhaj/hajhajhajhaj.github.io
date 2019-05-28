2f716c221be6646b8940f333dd3befbd276409b2
hajhajhajhaj <50372449+hajhajhajhaj@users.noreply.github.com>
2019-05-27 22:54:36 -0300

    Add files via upload

---
+++ AllanyContinuação/offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1559002353,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/personagem-sheet0.png",
+		"images/botão-sheet0.png",
+		"images/botão-sheet1.png",
+		"images/bala-sheet0.png",
+		"images/arrowdownbutton-sheet0.png",
+		"images/arrowdownbutton-sheet1.png",
+		"images/arrowupbotton-sheet0.png",
+		"images/arrowupbotton-sheet1.png",
+		"images/arrowleftbutton-sheet0.png",
+		"images/arrowleftbutton-sheet1.png",
+		"images/arrowrightbutton-sheet0.png",
+		"images/arrowrightbutton-sheet1.png",
+		"images/fundoanimado-sheet0.png",
+		"images/fundoanimado-sheet1.png",
+		"images/inimigo-sheet0.png",
+		"images/botondstroy-sheet0.png",
+		"images/barradevida-sheet0.png",
+		"images/barradevida2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
